ef42b6ca6126499a8e886d5a1ff663bc9a4e80e3
Bartek <bartosz.fluder@gmail.com>
2021-03-18 21:54:30 +0100

    JQuery file to enable dynamic form generation, file with defualt tags and categories for easy use.

---
+++ Shopper/Recipes/defualt_tags_&_categories.py
@@ -0,0 +1,5 @@
+categories = ['Płatki', 'Musli', 'Dżemy, miody, kremy', 'Konserwy i dania gotowe', 'Przetwory warzywne i owocowe',
+              'Sosy, oleje, ocet', 'Sypkie i produkty zbożowe', 'Przyprawy i dodatki kulinarne', 'Słodycze',
+              'Bakalie', 'Mięso', 'Mrożonki', 'Ryby']
+
+tags = ['Obiad', 'Kolacja', 'Wypieki', 'Śniadanie', 'Wege', 'Mięso', 'Ryby', 'Strączki']
